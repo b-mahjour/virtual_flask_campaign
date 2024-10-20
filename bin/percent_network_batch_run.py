574c83e9deeba95868b0aa5e252e32f5d65bd909
b-mahjour <bmahjour@umich.edu>
2024-10-19 21:18:34 -0400

    percent

---
+++ bin/percent_network_batch_run.py
@@ -0,0 +1,56 @@
+from shared.hpc.batch import SingleNetworkDistributedPipeline
+from rdkit import RDLogger
+import sys
+from shared.hpc.reset_tables import cancel_all_transactions, reset_psql
+RDLogger.DisableLog("rdApp.*")
+from virtual_flask.shared.hpc.commands import get_nodes, get_edges,get_network
+import psycopg2
+
+def connect_to_rds(
+    host="18.222.178.6",
+    port=6432,
+    dbname="testdb",
+    user="rxrange",
+    password="pass",
+):
+    """Create a connection to the PostgreSQL database"""
+    try:
+        conn = psycopg2.connect(
+            host=host, port=port, dbname=dbname, user=user, password=password
+        )
+        print("Connection established")
+        return conn
+    except Exception as e:
+        print(f"Unable to connect to the database: {e}")
+        return None
+
+
+conn = connect_to_rds()
+cur = conn.cursor()
+
+net = get_network(cur,1, "rxrange")
+nodes = get_nodes(cur,1, name="rxrange")
+edges = get_edges(cur,1, name="rxrange")
+net.load_data(nodes, edges)
+
+
+origin = nodes[0]
+print(origin)
+all_nodes_to_be_evald = []
+for node in nodes:
+    if node.other_data["structural_failure"] == False:
+        path = net.get_all_shortest_paths(origin.node_id, node.node_id)
+        for p in path:
+            for pp in p:
+                if pp not in all_nodes_to_be_evald:
+                    all_nodes_to_be_evald.append(pp)
+        node.other_data["path_from_origin"] = path
+print(len(all_nodes_to_be_evald))
+
+
+x = SingleNetworkDistributedPipeline("ten", all_nodes_to_be_evald[0:10], 10)
+import time 
+time0 = time.time()
+x.pipe("g16")
+
+print(time.time()-time0, "seconds")
