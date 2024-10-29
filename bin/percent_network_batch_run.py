e482a14818319beea2689c63f78a96e04c2c66f2
b-mahjour <bmahjour@umich.edu>
2024-10-28 21:46:21 -0400

    moreorca

--- bin/percent_network_batch_run.py
+++ bin/percent_network_batch_run.py
@@ -56,7 +56,7 @@ for node in nodes:
 print(len(all_nodes_to_be_evald))
 
 
-x = SingleNetworkDistributedPipeline("ten", all_nodes_to_be_evald[0:100], 100)
+x = SingleNetworkDistributedPipeline("ten", all_nodes_to_be_evald[100:400], 300)
 import time 
 time0 = time.time()
 x.pipe("g16")
